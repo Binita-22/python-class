58f0fe880a211b347ebccf901ced9ec705761084
Binita-22 <binita.adhikari.a23@icp.edu.np>
2025-06-28 22:21:33 +0545

    Exception handling

--- 007_exception _handling/main.py
+++ 007_exception _handling/main.py
@@ -2,8 +2,12 @@ try:
     a = int(input("Enter a number: "))
     output = 10 / a
     print(f"The output of user data is:{output} ")
-except Exception:
+except ZeroDivisionError:
+    # print(e.__class__.__name__)
     print("Invalid number!")
 # finally = code ma error awyani na awyani teyo chai run hunxas
 finally:
     print("Thank you for using our code.")
+# <-----------Note--------------------------------------------------->
+# exeception ko type tha huna lai used garinxa  e.__class__.__name__
+# where e stand for execption
